aab5f4758089c3f3fe3c38b25013a6dfe14e003c
app2641 <app2641@gmail.com>
2021-02-25 23:05:52 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :moon_age do
+    resource :tonight, only: :show
+  end
 end
