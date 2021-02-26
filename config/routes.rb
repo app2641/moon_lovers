cd90823a44cfc86dada1d076567e1e98ffa0a096
app2641 <app2641@gmail.com>
2021-02-26 21:58:38 +0900

    Root routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'moon_age/tonights#show'
+
   namespace :moon_age do
     resource :tonight, only: :show, default: { format: :json }
   end
