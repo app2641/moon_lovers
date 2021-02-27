c57e79ffcbbcab18ab2441989661da61bf23ccb9
app2641 <app2641@gmail.com>
2021-02-27 13:34:05 +0900

    Add comment

--- db/fixtures/moon_ages.rb
+++ db/fixtures/moon_ages.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# From https://eco.mtk.nao.ac.jp/koyomi/yoko/
 MoonAge.seed(
   :id,
   { id: 1, year: 2021, month: 1, state: 0, date: '2021/01/13', datetime: '2021/01/13 14:00' },
